a98348401ec784385f664d0a6c57b847b13260bf
bobllor <bobllor@yahoo.com>
2026-02-07 03:58:46 -0600

    added intunewin script

---
+++ intunewin-maker/make-intunewin.ps1
@@ -0,0 +1,46 @@
+<#
+	.DESCRIPTION
+	Creates the .intunewin file. This requires the IntuneWinAppUtil.exe executable to run.
+
+	.PARAMETERS
+		-$Source <string>
+			The target source folder. The output .intunewin file will also be created in this folder.
+
+		-SetupFile <string>
+			The target setup file.
+		
+	.AUTHOR
+	Tri Nguyen
+
+	.UDPATED
+	2/7/2026
+#>
+
+param(
+	[Parameter(Mandatory=$true)]
+	[string] $Source,
+	[Parameter(Mandatory=$true)]
+	[string] $SetupFile
+)
+
+$app = "IntuneWinAppUtil.exe"
+
+if(!(test-path $app)){
+	Write-Error "Missing $app in path $(pwd).path"
+	exit 1
+}
+
+if(test-path $Source -pathtype Container){
+	$folderName = ($Source -split "\\")[-2]
+	$fileName = ($SetupFile -split "\\")[-1]
+	
+	echo "Source: $Source"
+	echo "Setup file: $fileName"
+	echo "Output: $Source"
+	echo ""
+	
+	& ".\$app" -c "$Source" -s "$fileName" -o "$Source"
+}else{
+	Write-Error "Source path must be a directory"
+	exit 1
+}
